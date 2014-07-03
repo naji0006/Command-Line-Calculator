277a896a27e8a481db039bbe28e74e42f14235dc
naji0006 <naji0006@flinders.edu.au>
2014-07-03 20:46:49 +0930

    Added TEST 5 to 10 - See comments above each tests for further information.

--- OperationsTest.java
+++ OperationsTest.java
@@ -1,6 +1,7 @@
 package semestralka;
 
 import java.math.BigInteger;
+import java.util.Stack;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -10,12 +11,11 @@ import static org.junit.Assert.*;
  */
 public class OperationsTest {
 
-    public OperationsTest() {
-    }
+    public OperationsTest() {}
 
     /**
-     * TEST 1 Test of 'help' case in the doCommand method's switch statement, of
-     * class Operations.
+     * TEST 1 
+     * Test of 'help' case in the doCommand method's switch statement, of class Operations.
      *
      * Purpose: check to see if user input is case sensitive in 'help' case statement. 
      * Result: FAILED. 
@@ -31,8 +31,8 @@ public class OperationsTest {
     }
 
     /**
-     * TEST 2 Test of 'help' case in the doCommand method's switch statement, of
-     * class Operations.
+     * TEST 2 
+     * Test of 'help' case in the doCommand method's switch statement, of class Operations.
      *
      * Purpose: checking solution to correct the issue discovered in TEST 1. 
      * result: PASSED.
@@ -71,11 +71,135 @@ public class OperationsTest {
      * Result: Passed
      */
     @Test
-    public void SecondtTestFactorial() {
+    public void secondtTestFactorial() {
         int factorialValue = 5;
         int expectedValue = 120;
         BigInteger n = BigInteger.valueOf((long) factorialValue);
         int result = Operations.factorial(n).intValue();
         assertEquals(expectedValue, result);
+    }
+    
+    /**
+     * TEST 5
+     * Test of if statement in method isDouble, of class Operations.
+     *
+     * Purpose: checking if the statement can match the pattern fpRegex in the if statement. 
+     * Result: PASSED 
+     */
+    @Test
+    public void firstTestIsDouble() {
+        String token = "5.35";
+        boolean expResult = true;
+        boolean result = RPN_eval.isDouble(token);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * TEST 6
+     * Test of if statement in method isDouble, of class Operations.
+     *
+     * Purpose: checking if the statement can match the pattern fpRegex in the if statement. 
+     * Result: FAILED 
+     * Reason: program deem the token invalid according to pattern characters in fpRegex.
+     */
+    @Test
+    public void secondTestIsDouble() {
+        String token = "6/25";
+        boolean expResult = true;
+        boolean result = RPN_eval.isDouble(token);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * TEST 7
+     * Test of last if statement in method getDoubleFromStack, of class RPN_eval.
+     *
+     * Purpose: checking first alternate path of the if statement with first different condition.
+     * Result: FAILED
+     * Reason: first condition of first alternate path fails.
+     */
+    @Test
+    public void firstTestGetDoubleFromStack() {
+        Stack<String> stackTop = new Stack();
+        stackTop.push("5.00");
+        stackTop.push("+");
+        stackTop.push("1.00");
+        String stack = stackTop.pop();
+        RPN_eval test = new RPN_eval();
+        boolean expResult = false;
+        if (!test.isNumeric(stack) && test.isDouble(stack)) {
+            expResult = true;
+        }
+        assertEquals(expResult, true);
+    }
+
+    /**
+     * TEST 8
+     * Test of last if statement in method getDoubleFromStack, of class RPN_eval.
+     *
+     * Purpose: checking second alternate path of the if statement with second different condition.
+     * Result: FAILED
+     * Reason: second condition of second alternate path fails.
+     */
+    @Test
+    public void secondTestGetDoubleFromStack() {
+        Stack<String> stackTop = new Stack();
+        stackTop.push("9.00");
+        stackTop.push("+");
+        stackTop.push("3.00");
+        String stack = stackTop.pop();
+        RPN_eval test = new RPN_eval();
+        boolean expResult = false;
+        if (test.isNumeric(stack) && !test.isDouble(stack)) {
+            expResult = true;
+        }
+        assertEquals(expResult, true);
+    }
+    
+    /**
+     * TEST 9
+     * Test of last if statement in method getDoubleFromStack, of class RPN_eval.
+     *
+     * Purpose: checking third alternate path of the if statement with both conditions being different.
+     * Result: PASSED
+     */
+    @Test
+    public void thirdTestGetDoubleFromStack() {
+        Stack<String> stackTop = new Stack();
+        stackTop.push("4.00");
+        stackTop.push("+");
+        stackTop.push("6.00");
+        String stack = stackTop.pop();
+        RPN_eval test = new RPN_eval();
+        boolean expResult = false;
+        if (test.isNumeric(stack) && test.isDouble(stack)) {
+            expResult = true;
+        }
+        assertEquals(expResult, true);
+    }
+    
+    /**
+     * TEST 10
+     * Test of last if statement in method eval, of class RPN_eval.
+     *
+     * Purpose: testing alternate path to the condition stack.size() != 1. 
+     * Result: FAILED
+     * Reason: Condition fails due to the size of stack being not eqaul to one.
+     */
+    @Test
+    public void evalTest() {
+        Stack<String> stack = new Stack();
+        stack.push("RAD");
+        stack.push("x");
+        stack.push("=");
+        stack.push("5");
+        stack.push("!");
+        stack.push("+");
+        stack.push("1");
+        boolean expResult = false;
+        if (stack.size() == 1) {
+            expResult = true;
+        }
+        assertEquals(expResult, true);
     }
 }
